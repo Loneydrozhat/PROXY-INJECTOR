22401a202c0c988d7f60f3739eba4871e90ab14b
FENIX TM SCRIPTS <walerson.zoin@gmail.com>
2017-11-14 22:01:07 -0200

    @PYTHON-PROXY

--- proxy.py
+++ proxy.py
@@ -21,7 +21,7 @@ import subprocess
 subprocess.call("clear",shell=True)
 
 server = "127.0.0.1"
-msg1 = 'SOCKET OK'
+msg1 = 'SOCKS'
 msg2 = 'Erro, Proxy nao eh publico'
 class ThreadingHTTPServer(ThreadingMixIn, HTTPServer):
 
@@ -73,7 +73,7 @@ class SimpleHTTPProxyHandler(BaseHTTPRequestHandler):
         except socket.error:
             return
 
-        self.send_response(200, msg1 )
+        self.send_response(200, msg1)
         self.send_header('Connection', 'close')
         self.end_headers()
 
@@ -81,7 +81,7 @@ class SimpleHTTPProxyHandler(BaseHTTPRequestHandler):
         keep_connection = True
         while keep_connection:
             if not server.find(hostip) != -1:
-                self.send_error(403, msg2 )
+                self.send_error(403, msg2)
                 self.close_connection
             keep_connection = False
             rlist, wlist, xlist = select.select(conns, [], conns, self.timeout)
